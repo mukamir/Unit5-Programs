fe56e7ecb2bc1d92a36fb98236332bde71ce9e96
mukamir <am25524@email.vccs.edu>
2015-04-13 14:51:08 -0400

    Unit5 Programs 1-4

---
+++ Unit5.Programs/src/GridLayoutDemo.java
@@ -0,0 +1,148 @@
+/*
+ * 
+ * 
+ * 
+ */
+
+import java.awt.*;
+import javax.swing.*;
+
+public class GridLayoutDemo extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+
+	/*** Class Methods ***/
+	
+	public GridLayoutDemo()
+	{
+		super( "GridLayout Demo" );
+		
+		/*** Build GUI ***/
+		
+		buildGUI();
+		
+		/*** Set JFrame attributes ***/
+		
+		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		setSize( 300, 250 );
+		center();
+		pack();
+		setVisible( true );
+	}
+	
+	/*** Class helper methods ***/
+	
+	private void center()
+	{
+		/*** Local Variables ***/
+		
+		Dimension dim = null;
+		int frameWidth;
+		int frameHeight;
+		int frameLocationX;
+		int frameLocationY;
+				
+		/*** Get the size of the primary screen being usd to display GUI ***/
+		
+		dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+		/*** Calculate the new location of the container (JFrame) ***/
+		
+		frameWidth  = getSize().width;
+		frameHeight = getSize().height;
+
+		frameLocationX = ( dim.width  - frameWidth  ) / 2;
+		frameLocationY = ( dim.height - frameHeight ) / 2;
+
+		/*** Center container by setting upper left hand corner on screen ***/
+		
+		setLocation( frameLocationX, frameLocationY ); 
+	}
+
+	private void buildGUI()
+	{
+		/*** Local Variables ***/
+		
+		JPanel jpaPanel1 = null;
+		JPanel jpaPanel2 = null;
+		
+		/*** Instantiate GUI Components ***/
+		
+		setLayout( new BorderLayout() );
+		
+		jpaPanel1 = createPanel1();
+		jpaPanel2 = createPanel2();	
+		
+		/*** Add GUI Components to the container (JFrame) ***/
+		
+		add( "Center", jpaPanel2 );
+		add( "South",  jpaPanel1 );
+	}
+	
+	private JPanel createPanel1()
+	{	
+		/*** Local Variables ***/
+		
+		JPanel jpaPanel1 = null;
+		
+		JButton jbuButton1 = null;	
+		JButton jbuButton2 = null;
+		JButton jbuButton3 = null;
+		
+		/*** Instantiate GUI Components ***/
+		
+		jpaPanel1 = new JPanel();
+		jpaPanel1.setLayout( new GridLayout( 2, 2 ) );
+		
+		jbuButton1 = new JButton( "Button 1" );
+		jbuButton2 = new JButton( "Button 2" );
+		jbuButton3 = new JButton( "Button 3" );
+		
+		/*** Add GUI components to container (JPanel) ***/
+		
+		jpaPanel1.add( jbuButton1 );
+		jpaPanel1.add( jbuButton2 );
+		jpaPanel1.add( jbuButton3 );
+		
+		/*** Return container ***/
+		
+		return jpaPanel1;
+	}
+	
+	private JPanel createPanel2()
+	{
+		/*** Local Variables ***/
+		
+		JPanel jpaPanel2 = null;
+		
+		JButton jbuButton4 = null;	
+		JButton jbuButton5 = null;
+		JButton jbuButton6 = null;
+		
+		/*** Instantiate GUI Componenets ***/
+		
+		jpaPanel2 = new JPanel();
+		jpaPanel2.setLayout( new GridLayout( 2, 2 ) );
+		
+		jbuButton4 = new JButton( "Button 4" );
+		jbuButton5 = new JButton( "Button 5" );
+		jbuButton6 = new JButton( "Button 6" );
+		
+		/*** Add GUI components to container (JPanel) ***/
+		
+		jpaPanel2.add( jbuButton4 );
+		jpaPanel2.add( jbuButton5 );
+		jpaPanel2.add( jbuButton6 );
+		
+		/*** Return container ***/
+		
+		return jpaPanel2;
+	}
+	
+	/*** Application ***/
+	
+	public static void main( String args[] )
+	{
+		new GridLayoutDemo(); 
+	}
+}
---
+++ Unit5.Programs/src/Parabola.java
@@ -0,0 +1,125 @@
+/*
+ * 
+ * 
+ * 
+ */
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Parabola extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+
+	/*** Class Methods ***/
+	
+	public Parabola()
+	{
+		super( "Parabola" );
+		
+		/*** Build GUI ***/
+		
+		buildGUI();
+		
+		/*** Set JFrame attributes ***/
+		
+		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		setSize( 500, 500 );
+		center();
+		//pack();
+		setVisible( true );
+	}
+	
+	/*** Inner class GraphPenel ***/
+	
+	private class GraphPanel extends JPanel
+	{
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		protected void paintComponent( Graphics g )
+		{
+			/*** Local Variables ***/
+		
+			Polygon polygon = new Polygon();
+			double scaleFactor = 0.1;
+			int xCoordinate = 0;			
+			int yCoordinate = 0;
+			
+			/*** Add points to polygon object ***/
+			
+			for ( int x = -100; x <= 100; x++ )
+			{
+			   xCoordinate = x + 200;
+			   yCoordinate = 200 - (int) (scaleFactor * x * x );
+			   
+			   polygon.addPoint( xCoordinate, yCoordinate );
+			}
+			
+			/*** Draw x and y axis ***/
+			
+			g.drawLine(   0, 200, 400, 200 );  // x axis			
+			g.drawLine( 200,   0, 200, 400 );  // y axis
+			
+			/*** Draw x and y labels ***/
+			
+			g.drawString( "x axis", 300, 180  );
+			g.drawString( "y axis", 220, 300 );
+			
+			/*** Draw parabola graph on screen ***/
+			
+			g.drawPolyline( polygon.xpoints, polygon.ypoints, polygon.npoints );
+		}
+	}
+	
+	/*** Class helper methods ***/
+	
+	private void center()
+	{
+		/*** Local Variables ***/
+		
+		Dimension dim = null;
+		int frameWidth;
+		int frameHeight;
+		int frameLocationX;
+		int frameLocationY;
+				
+		/*** Get the size of the primary screen being usd to display GUI ***/
+		
+		dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+		/*** Calculate the new location of the container (JFrame) ***/
+		
+		frameWidth  = getSize().width;
+		frameHeight = getSize().height;
+
+		frameLocationX = ( dim.width  - frameWidth  ) / 2;
+		frameLocationY = ( dim.height - frameHeight ) / 2;
+
+		/*** Center container by setting upper left hand corner on screen ***/
+		
+		setLocation( frameLocationX, frameLocationY ); 
+	}
+
+	private void buildGUI()
+	{
+		/*** Local Variables ***/
+		
+		JPanel jpaGraphPanel = null;
+		
+		/*** Instantiate GUI Components ***/
+		
+		jpaGraphPanel = new GraphPanel();
+		
+		/*** Add GUI Components to the container (JFrame) ***/
+		
+		add( jpaGraphPanel );
+	}
+	
+	/*** Application ***/
+	
+	public static void main( String args[] )
+	{
+		new Parabola(); 
+	}
+}
